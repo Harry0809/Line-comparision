6788e460f79dfb49fcf4cb29efed935df7b47ccd
Harish mahur <hmahur20@gmail.com>
2022-07-17 10:30:36 +0530

    modified: compare lines-equal greater or less than

--- LineComparision.java
+++ LineComparision.java
@@ -1,13 +1,13 @@
 package com.line.comparision;
 
-public class LineComparision {
+
+public class LineComparision{
 
 	int x1, x2, y1, y2;
 	int a1, a2, b1, b2;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		System.out.println("Welcome to Line Comparision");
 		LineComparision method = new LineComparision();
 		method.Length();
@@ -15,26 +15,31 @@ public class LineComparision {
 
 	public void Length() {
 		Double distance_a, distance_b;
-		x1 = 1;
-		y1 = 5;
-		x2 = 6;
-		y2 = 6;
-		a1 = 1;
-		b1 = 5;
-		a2 = 7;
-		b2 = 6;
+		x1 = 2;
+		y1 = 3;
+		x2 = 2;
+		y2 = 3;
+		a1 = 2;
+		b1 = 3;
+		a2 = 2;
+		b2 = 3;
+
 		distance_a = Math.sqrt(Math.pow((x2 - x1), 2) + (Math.pow((y2 - y1), 2)));
 		System.out.println("Calculated len1" + distance_a);
 		distance_b = Math.sqrt(Math.pow((a2 - a1), 2) + (Math.pow((b2 - b1), 2)));
 		System.out.println("Calculated len2:" + distance_b);
-		if (distance_a.equals(distance_b)) {
-			System.out.println("Line are Equal");
+		if (distance_a.compareTo(distance_b) == 0) {
+			System.out.println("Line1 is  Equal to Line2");
 
-		} else {
-			System.out.println("Line are Not Equal");
+		} else if (distance_a.compareTo(distance_b) == -1) {
+			System.out.println("Line1 less than Line2");
+		} else if (distance_a.compareTo(distance_b) == +1) {
+			System.out.println("Line1 Greater than Line2");
 		}
-
 	}
 
-
 }
+
+
+
+
